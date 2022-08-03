384331d27a9552f369f4b8c4d825a3956eb667e3
hujiayucc <hujiayucc@qq.com>
2022-08-03 13:18:07 +0800

    颜色列表

---
+++ Lib/Color.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @Author: hujiayucc
+ * @Date: 2022-07-30 07:23:36
+ * @Description: 颜色列表
+ */
+
+const 红色 = 255;
+const 白色 = 16777215;
+const 黑色 = 0;
+const 蓝色 = 16711680;
+const 天蓝 = 16746496;
+const 粉红 = 16754943;
+const 黄色 = 65535;
+const 绿色 = 65280;
+const 墨绿 = 32768;
+const 灰色 = 8421504;
