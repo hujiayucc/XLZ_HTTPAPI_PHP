894f2051b12be7c4c5fab0d9a7eca15cd0d680d3
hujiayucc <hujiayucc@qq.com>
2022-08-03 13:18:17 +0800

    配置文件

---
+++ Lib/Config.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * @Author: hujiayucc
+ * @Date: 2022-07-26 03:55:34
+ * @Description: 配置文件
+ */
+
+/**
+ * @var string HTTP API地址
+ */
+const CONFIG_HOST = "http://127.0.0.1:10429";
+
+/**
+ * @var string 用户名
+ */
+const CONFIG_USER = "ceshi";
+
+/**
+ * @var string 密码
+ */
+const CONFIG_PASSWD = "ceshi";
+
+/**
+ * @var string 请求浏览器UA
+ */
+const CONFIG_UA = "hujiayucc";
+
+/**
+ * @var string Content-Type验证
+ */
+const CONTENT_TYPE = "hujiayucc";
