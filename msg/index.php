ae54ec58fa39b3086ed9657b61a1c26eefaf92d8
hujiayucc <hujiayucc@qq.com>
2022-08-03 13:19:34 +0800

    事件回调

---
+++ msg/index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * @Author: hujiayucc
+ * @Date: 2022-08-02 10:01:47
+ * @Description: 事件回调 http(s)://127.0.0.1/msg/
+ */
+
+require_once("../Lib/init.php");
+//Header校验
+if ($_SERVER["CONTENT_TYPE"] != CONTENT_TYPE) exit(header("HTTP/1.1 404 Not Found"));
+$msg = json_decode(file_get_contents("php://input"));
+$msgtype = $msg->type;
+switch ($msgtype) {
+    case 事件类型_私聊消息:
+        事件类型_私聊消息($msg);
+        break;
+    case 事件类型_群聊消息:
+        事件类型_群聊消息($msg);
+        break;
+    case 事件类型_事件:
+        # code...
+        break;
+    case 事件类型_频道推送数据:
+        # code...
+        break;
+    default:
+        header("HTTP/1.1 404 Not Found");
+        break;
+}
+
+function 事件类型_私聊消息($msg)
+{
+    define('私聊_框架QQ', $msg->logonqq);
+    define('私聊_消息QQ', $msg->fromqq->qq);
+    define('私聊_消息内容', $msg->msg->text);
+    doAction('private', 私聊_框架QQ, 私聊_消息QQ, 私聊_消息内容);
+}
+
+function 事件类型_群聊消息($msg)
+{
+    define('群聊_框架QQ', $msg->logonqq);
+    define('群聊_消息群号', $msg->fromgroup->group);
+    define('群聊_消息QQ', $msg->fromqq->qq);
+    define('群聊_消息内容', $msg->msg->msg);
+    define('群聊_消息Req', $msg->msg->req);
+    define('群聊_消息Random', $msg->msg->random);
+    doAction('group', 群聊_框架QQ, 群聊_消息群号, 群聊_消息QQ, 群聊_消息内容, 群聊_消息Req, 群聊_消息Random);
+}
