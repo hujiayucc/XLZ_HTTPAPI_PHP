bff2bee78f1aaf2ad0cb4cfb3b6b513873f3fadf
hujiayucc <hujiayucc@qq.com>
2022-08-05 17:50:30 +0800

    新增闪照破解模板

--- plugins/群插件模板/群插件模板.php
+++ plugins/群插件模板/群插件模板.php
@@ -9,7 +9,7 @@ require_once("../Lib/init.php");
 
 function 关键词撤回($robot, $group, $toqq, $msg, $req, $random)
 {
-    if ($msg == "23333")
+    if ($msg == "hujiayucc" )
         API::撤回消息($robot, 消息类型_群聊消息, $toqq, $group, 留空, $random, $req);
 }
 //group群聊挂载点
@@ -17,7 +17,21 @@ addAction('group', '关键词撤回');
 
 function 群关键词回复($robot, $group, $toqq, $msg, $req, $random)
 {
-    if ($msg == "123")
-        API::发送群消息($robot, $group, "666");
+    if ($msg == "小宇最帅" && $robot != $toqq)
+        API::发送群消息($robot, $group, $msg);
 }
 addAction('group', '群关键词回复');
+
+function 闪照破解($robot, $group, $toqq, $msg)
+{
+    $flash = "[flashPic,hash=";
+    if ($toqq != $robot) {
+        if ($group == 703195149 || $group == 437932024 || $group == 936549039) {
+            if (strpos($msg, $flash) !== false) {
+                $message = "[@" . $toqq . "] 发送了一张闪照\n\n" . str_replace("flashPic", "pic", $msg);
+                API::发送群消息($robot, $group, $message);
+            }
+        }
+    }
+}
+addAction('group', '闪照破解');
