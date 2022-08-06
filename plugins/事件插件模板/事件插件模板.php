978f997e39e68328d38990b6828d3f46ec41a094
hujiayucc <hujiayucc@qq.com>
2022-08-06 17:32:50 +0800

    新增插件模板

--- plugins/事件插件模板/事件插件模板.php
+++ plugins/事件插件模板/事件插件模板.php
@@ -7,12 +7,25 @@
 
 function 防撤回(int $robbot, int $group, int $toqq, string $type, string $msg)
 {
-    if($robbot != $toqq && $type == "群事件_某人撤回事件"){
-        if ($group == 703195149 || $group == 437932024 || $group == 936549039) {
-            $message = "[@" . $toqq . "] 撤回了一条消息\n\n" . $msg;
+    if ($robbot != $toqq && $type == "群事件_某人撤回事件") {
+        if ($group == 703195149 || $group == 437932024 || $group == 936549039 || $group == 590201901) {
+            $message = 文本代码::艾特($toqq) . "撤回了一条消息\n\n" . $msg;
             API::发送群消息($robbot, $group, $message);
         }
     }
 }
 //event事件挂载点
 addAction('event', '防撤回');
+
+function 禁言嘲讽(int $robbot, int $group, int $toqq, string $type)
+{
+    if ($type == "群事件_某人被禁言") {
+        if ($group == 703195149 || $group == 437932024 || $group == 936549039 || $group == 590201901)
+            API::发送群消息($robbot, $group, 文本代码::艾特($toqq) . "叼毛被禁言了吧，我们一起嘲笑他！");
+    }
+    if ($type == "群事件_某人被解除禁言") {
+        if ($group == 703195149 || $group == 437932024 || $group == 936549039 || $group == 590201901)
+            API::发送群消息($robbot, $group, 文本代码::艾特($toqq) . "饶你一条狗命，看你下次还敢不敢");
+    }
+}
+addAction('event', '禁言嘲讽');
